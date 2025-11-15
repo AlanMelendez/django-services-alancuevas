3d388112317fcf180330a65b9fac67d4fb2d682c
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-11-14 20:44:11 -0600

    add: implement text embedding function and integrate it into prompt creation

--- backend/prompts/views.py
+++ backend/prompts/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
+from backend.prompts.embeddings import text_to_embedding
+
 from .models import Prompt
 from .serializers import PromptSerializer
 from .throttles import BurstRateThrottle, SustainedRateThrottle
@@ -17,11 +19,14 @@ class PromptCreateView(APIView):
             return Response({"error": "prompts is required"}, status=400)
 
         generated_response = f"Mockedai response to: {prompt_text}"
+        embedding_vector = text_to_embedding(prompt_text).tolist() # it's to make it JSON serializable when you use the JSONField
+
 
         obj = Prompt.objects.create(
             user=request.user,
             prompt=prompt_text,
-            response=generated_response
+            response=generated_response,
+            embedding=embedding_vector
         )
 
         return Response(PromptSerializer(obj).data, status=201)
