cd1751c59d5d46e1e50979c6b3491ddb909dd34b
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-11-15 01:15:06 -0600

    add: configure logging settings for Django application

--- backend/django_services_alancuevas/settings.py
+++ backend/django_services_alancuevas/settings.py
@@ -168,4 +168,25 @@ CHANNEL_LAYERS = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
 
